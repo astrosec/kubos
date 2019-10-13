53a4138f46372de7b98b8dde4daa775a0b35db30
Frank <frank.pound@astrosec.com>
2019-10-12 23:52:35 -0700

    -Removed the -r argument since 'OnCommand' and friends are deprecated -Removed parsing of -r from the c code -Simplified the mission application since we no longer have to strip the oddball '--' -Added examples in register.txt to use with GraphiQL tool

--- examples/kubos-linux-bbb-leds/source/kubos-makeLED.py
+++ examples/kubos-linux-bbb-leds/source/kubos-makeLED.py
@@ -4,31 +4,12 @@ import argparse
 import app_api
 import sys
 
-
-#SERVICES = app_api.Services()
-
-def on_boot(logger):
-
-    logger.info("OnBoot logic")
-
-def on_stop(logger):
-
-    logger.info("OnStop logic")
-    try:
-        cmd = ['/home/kubos/blinkleds/kubos-linux-makeLED' , "-r", "off"]
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        o,e = proc.communicate()
-        logger.info("LED STOP " + o.decode('ascii'))
-    except Exception as ex:
-        logger.error("Something went wrong in LED blinker: " + str(ex) + str(e.decode('ascii')) + str(proc.returncode) + "\r\n")
-        logger.error("LED ERROR ")
-        sys.exit(1)
-
 def on_command(logger,subcommand):
 
-    logger.info("OnCommand logic" +  str(subcommand))
+    logger.info("OnCommand logic" +  subcommand)
+
     try:
-        cmd = ['/home/kubos/blinkleds/kubos-linux-makeLED' , '-r', str(subcommand)]
+        cmd = ['/home/kubos/blinkleds/kubos-linux-makeLED' , subcommand]
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         o,e = proc.communicate()
         logger.info("LED FLASH " + o.decode('ascii'))
@@ -42,28 +23,38 @@ def main():
     logger = app_api.logging_setup("kubos-linux-makeLED")
 
     logger.info("Entering main LED logic BLINKER!!! ")
-    theargs = str(sys.argv[1:])
-    theargs.replace("--","")
-    logger.info("args are " + theargs)
 
-    if "--" in sys.argv: sys.argv.remove("--")
+    parser = argparse.ArgumentParser()
 
-    parser = argparse.ArgumentParser(theargs)
+    parser.add_argument(
+        '-c',
+        '--config',
+        nargs=1,
+        help='Specifies the location of a non-default configuration file')
 
-    parser.add_argument('--run', '-r')
-    parser.add_argument('--subcommand', '-s')
+    parser.add_argument(
+        '-s',
+        '--subcommand',
+        help='on, off, flash')
 
     args = parser.parse_args()
 
-    logger.info("args are " + str(args))
-
-    if args.run == 'OnBoot':
-        on_boot(logger)
-    elif args.run == 'OnCommand':
-        on_command(logger,args.subcommand)
+    if args.config is not None:
+        global SERVICES
+        SERVICES = app_api.Services(args.config[0])
     else:
-        logger.error("Unknown run level specified")
-        sys.exit(1)
+        SERVICES = app_api.Services()     
+
+    logger.info("Command to LED Blinker is " + str(args.subcommand))
+
+    if args.subcommand == 'flash':
+        on_command(logger,'flash')
+    elif args.subcommand == 'off':
+        on_command(logger,'off')
+    elif args.subcommand == 'on':
+        on_command(logger,'on')
+    else:
+        logger.error("Bad command to led blinker " + args.subcommand)
 
 if __name__ == "__main__":
     main()
